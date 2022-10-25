19a20212445a2e93c37c72bf83aa0b0390ec8b5c
mazenwkamel-auc <mazenwkamel@aucegypt.edu>
2022-10-25 12:23:38 +0200

    Implemented Book data

--- Library-System/bookdata.h
+++ Library-System/bookdata.h
@@ -1,11 +1,21 @@
 #ifndef BOOKDATA_H
 #define BOOKDATA_H
 
+#include <QString>
+
 
 class BookData
 {
 public:
-    BookData();
+    BookData(QString _name, int _price);
+    void setName(QString _name);
+    QString getName();
+    void setPrice(int _price);
+    int getPrice();
+
+private:
+    QString name{};
+    int price{};
 };
 
 #endif // BOOKDATA_H
