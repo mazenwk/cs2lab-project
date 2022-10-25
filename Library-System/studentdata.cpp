f5558da72b8bf8c7cf0c4b810b02cd5b2434be3b
HabibaSeif <habibaseif@aucegypt.edu>
2022-10-25 13:34:53 +0300

    Student Data

--- Library-System/studentdata.cpp
+++ Library-System/studentdata.cpp
@@ -7,5 +7,40 @@ StudentData::StudentData()
 
 void StudentData::setName(QString n)
 {
+    Name = n;
+}
 
+void StudentData::setPassword(QString p)
+{
+    password = p;
+}
+
+void StudentData::setId(QString id)
+{
+    Id = id;
+}
+
+void StudentData::setAccountBalance(double AB)
+{
+    AccountBalance = AB;
+}
+
+QString StudentData::getName()
+{
+    return Name;
+}
+
+QString StudentData::getPassword()
+{
+    return password;
+}
+
+QString StudentData::getId()
+{
+    return Id;
+}
+
+double StudentData::getAB()
+{
+    return AccountBalance;
 }
--- Library-System/studentdata.h
+++ Library-System/studentdata.h
@@ -1,11 +1,28 @@
 #ifndef STUDENTDATA_H
 #define STUDENTDATA_H
-
+#include <QString>
+#include "bookdata.h"
 
 class StudentData
 {
+private:
+    QString Name = " ";
+    QString password = " ";
+    QString Id = " ";
+    BookData *PurchasedBooks;
+    double AccountBalance = 0;
+
+
 public:
     StudentData();
+    void setName(QString n);
+    void setPassword(QString p);
+    void setId(QString id);
+    void setAccountBalance(double AB);
+    QString getName();
+    QString getPassword();
+    QString getId();
+    double getAB();
 };
 
 #endif // STUDENTDATA_H
